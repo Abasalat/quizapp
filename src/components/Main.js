255cda97e1fcbcfb64a397662e594730e408d0a1
afifaqazi <154233702+afifaqazi@users.noreply.github.com>
2024-01-13 23:21:33 +0500

    commit by afifa qazi

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,71 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
-import '../style/Main.css'
+import React, { useRef } from "react";
+import { Link, useLocation, Route, Routes } from "react-router-dom";
+import "../style/Main.css";
+
+import Quiz from "./Quizes";
 
 const Main = () => {
-    const inputRef = useRef(null)
-  return (
-    <div className='container'>
-      <h1 className=' title text-light'>Quiz Application</h1>
-      <div className='Mainbox'>
-      <div className='box b1'>
-        <h3>Genaral Knowlege</h3>
-        <div className='start s1'>
-        <Link className='btn' to={'quiz'}>Start Quiz</Link>
-      </div>
-      </div>
-      <div className='box b2'>
-        <h3>Sports</h3>
-        <div className='start s2'>
-        <Link className='btn' to={'quiz2'}>Start Quiz</Link>
-      </div>
-      </div>
-      <div className='box b3'>
-        <h3>Science</h3>
-        <div className='start s3'>
-        <Link className='btn' to={'quiz3'}>Start Quiz</Link>
-      </div>
-      </div>
-      <div className='box b4'>
-        <h3>Programming</h3>
-        <div className='start s4'>
-        <Link className='btn' to={'quiz4'}>Start Quiz</Link>
-      </div>
-      </div>
-      
-      </div>
-      
+  const inputRef = useRef(null);
 
-      <form id='form' >
-      <input ref={inputRef} type='text'  className='userid' placeholder='UserName*'/>
+  const location = useLocation();
+  return (
+    <div className="container">
+      <h3 style={{ color: "white" }}>
+        Welcome {location.state && location.state.id}
+      </h3>
+      <h1 className=" title text-light">Quiz Application</h1>
+      <div className="Mainbox">
+        <div className="box b1">
+          <h3>Genaral Knowlege</h3>
+          <div className="start s1">
+            <Link className="btn" to={"/quiz"}>
+              Start Quiz
+            </Link>
+          </div>
+        </div>
+        <div className="box b2">
+          <h3>Sports</h3>
+          <div className="start s2">
+            <Link className="btn" to={"/quiz2"}>
+              Start Quiz
+            </Link>
+          </div>
+        </div>
+        <div className="box b3">
+          <h3>Science</h3>
+          <div className="start s3">
+            <Link className="btn" to={"quiz3"}>
+              Start Quiz
+            </Link>
+          </div>
+        </div>
+        <div className="box b4">
+          <h3>Programming</h3>
+          <div className="start s4">
+            <Link className="btn" to={"/quiz4"}>
+              Start Quiz
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <form id="form">
+        <input
+          ref={inputRef}
+          type="text"
+          className="userid"
+          placeholder="UserName*"
+        />
       </form>
 
-      
+      <Routes>
+        <Route path="/quiz" element={<Quiz quizName="General Knowledge" />} />
+        <Route path="/quiz2" element={<Quiz quizName="Sports" />} />
+        <Route path="/quiz3" element={<Quiz quizName="Science" />} />
+        <Route path="/quiz4" element={<Quiz quizName="Programming" />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
